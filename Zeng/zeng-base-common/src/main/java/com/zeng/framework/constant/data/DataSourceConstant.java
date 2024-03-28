014678a6c6e3458b4ea8c16ff198ce4ce040315b
VcrTing <vcrting@163.com>
2024-03-28 15:59:53 +0800

    0

--- Zeng/zeng-base-common/src/main/java/com/zeng/framework/constant/data/DataSourceConstant.java
+++
@@ -1,8 +0,0 @@
-package com.zeng.framework.constant.data;
-
-public interface DataSourceConstant {
-
-    String MASTER = "master";
-    String MULTI_DB_1 = "multi-one";
-    String MULTI_DB_2 = "multi-two";
-}
